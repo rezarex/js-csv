60df1d6af6dbfbd00628d7ffc0f75db37413e80e
rezarex <marvin.wekesa@gmail.com>
2023-07-29 15:32:25 +0300

    csv-json

---
+++ jsparser.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+csvfile = fs.readFileSync('./simple.csv')
+const arr = csvfile.toString().split('\n') //convert the csv to string
+//console.log(arr);
+var jsonObj = []
+var headers = arr[0].split(",")  //get the title/header
+//console.log(headers);
+for(var i=1; i<arr.length;i++){
+    var data = arr[i].split(',')
+    var object = {};
+    for(var j=0; j<data.length;j++){
+        object[headers[j].trim()] = data[j].trim();
+    }
+    jsonObj.push(object)
+}
+
+console.log(JSON.stringify(jsonObj));
